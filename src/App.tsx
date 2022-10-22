ae6c11852d77fd7239d811afaa1f27c9f40c73b3
Bhumika Sharma <bhumikasharma@jklu.edu.in>
2022-10-23 02:08:02 +0530

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import type { Component } from 'solid-js'
 
 const App: Component = () => {
-    return (
-        <div>
-            <p>Solid JS Application</p>
-            <p>JWi-Fi is a login wifi login eliminator</p>
-            <p>The user has been changed</p>
-            <button>go on nice</button>
-        </div>
-    )
+  return (
+    <div>
+      <p>Solid JS Application</p>
+      <p>JWi-Fi is a login wifi login eliminator</p>
+      <p>The user has been changed</p>
+      <button>go on nice1</button>
+    </div>
+  )
 }
 
 export default App
