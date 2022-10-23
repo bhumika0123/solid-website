e90825b219ab7841ca206746ac3cc26c268f4675
Aditya Raj Kumawat <arkumawat78@gmail.com>
2022-10-24 00:43:17 +0530

    changed design

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,23 @@ const App: Component = () => {
   return (
     <div class='px-28 py-14'>
       <div class='text-right underline'>wtf?</div>
-      <div class='flex justify-between py-20'>
-        <div class='flex flex-col w-1/2 justify-center pb-28'>
-          <div class='flex py-8 items-center'>
-            <div class='mr-10 border-neutral-700 border-2 px-8 py-3 rounded-xl text-xl'>
-              JWi-Fi
+      <div class='flex justify-center text-center py-20 mt-40'>
+        <div class='flex flex-col justify-center items-center w-1/3'>
+          <div class='flex py-8 items-center justify-center relative w-full'>
+            <div class='mr-10 border-neutral-700 border-2 px-10 py-3 rounded-xl text-2xl'>
+              <p>jwifi</p>
             </div>
-            <p class='text-zinc-500'>developed by @edydee</p>
+            {/* <p class='text-zinc-500 absolute text-base'>developed by @edydee</p> */}
           </div>
           <div class='mb-3'>
-            <p class='text-xl'>
+            <p class='text-lg'>
               Logging in college's WiFi every now and then could be annoying at
               times, especially when you need to type in credentials each time.
             </p>
           </div>
 
-          <div class='mb-4'>
-            <p class='text-xl'>
+          <div class='mb-5'>
+            <p class='text-lg'>
               So, here is a solution for our university WiFi, a tool that will
               automatically log you in each time you start your system.
             </p>
@@ -30,13 +30,13 @@ const App: Component = () => {
 
           <div>
             <button class='bg-black text-white px-5 py-3 rounded-lg'>
-              Download for Debian
+              Download for MacOS
             </button>
           </div>
         </div>
-        <div>
-          <img src={right} alt='' />
-        </div>
+        {/* <div class='relative w-1/2'>
+          <img class='absolute top-0 left-0' src={right} alt='' />
+        </div> */}
       </div>
     </div>
   )
